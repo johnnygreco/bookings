0bddc60b489a2a70dd641b101c91a7c343db4a69
Johnny Greco <jgreco.ai@gmail.com>
2022-06-02 16:00:29 -0400

    initial commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"github.com/johnnygreco/bookings/pkg/config"
+	"github.com/johnnygreco/bookings/pkg/models"
+	"github.com/johnnygreco/bookings/pkg/render"
+	"net/http"
+)
+
+// The repository used by the handlers
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the homepage handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
