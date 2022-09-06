cbfdb83318adbfc08bcc707fff0d6cfe6a4b6e7f
Johnny Greco <jgreco.ai@gmail.com>
2022-09-05 20:30:38 -0400

    search room availability method added

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,12 +1,15 @@
 package repository
 
 import (
+	"time"
+
 	"github.com/johnnygreco/bookings/internal/models"
 )
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
-	InsertReservation(res models.Reservation) (int, error) 
+	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
